e262e32e17461e5517b5017dd9e39962f67d8f90
Sparsh Mehta <mehtasparsh777@gmail.com>
2023-11-24 23:59:59 +0530

    Day 4

--- assignments/todoserver/index.js
+++ assignments/todoserver/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var fs = require("fs");
 var app = express();
 var port = 3000;
 var todos = [];
@@ -8,6 +9,19 @@ var id = 0;
 function getTodos(req, res) {
   res.status(200).send(todos);
 }
+function submit(data) {
+  console.log(data);
+}
+function addToFile(data) {
+  fs.writeFile("./todo.txt", JSON.stringify(data), (err) => {
+    console.log(err);
+  });
+  fs.readFile("./todo.txt", (d) => {
+    console.log(d);
+    console.log("read successfully");
+  });
+}
+
 function createTodo(req, res) {
   const body = req.body;
   id += 1;
@@ -17,6 +31,7 @@ function createTodo(req, res) {
     description: body.description,
     id: id,
   });
+  addToFile(todos);
   var response = { id: id };
   res.status(201).send(response);
 }
@@ -26,6 +41,11 @@ function handleFirst(req, res) {
 }
 
 function getTodoById(req, res) {
+  var data;
+  fs.readFile("./todo.txt", (d) => {
+    data = JSON.parse(d);
+  });
+  console.log(data);
   res.status(200).send(todos[req.params.id - 1]);
 }
 
@@ -38,12 +58,10 @@ function updateTodoById(req, res) {
   };
   res.status(200).send({ id: req.params.id, updated: true });
 }
-function checkId(id, data) {
-  return data.id !== Number(id);
-}
+
 function deleteTodoById(req, res) {
   var id = req.params.id;
-  todos = todos.filter((v) => checkId(id, v));
+  todos = todos.filter((v) => data.id !== Number(id));
   res.status(200).send({ id: id, deleted: true });
 }
 app.use(bodyParser.json());
